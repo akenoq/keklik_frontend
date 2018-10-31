4b8bdf545d93cfed3b60706b018dccb6da56ee2a
akenoq <akenoq@yandex.ru>
2018-10-31 04:37:28 +0300

    add render group by id

--- static/views/organization/OrganizationPage.js
+++ static/views/organization/OrganizationPage.js
@@ -13,9 +13,15 @@ export default class OrganizationPage extends Page {
     constructor() {
         super();
         this.addEventsOnButtons();
+
+        let btnToGroup = document.createElement('button');
+        btnToGroup.setAttribute('id', 'to-group-btn-redirect');
+        btnToGroup.hidden = true;
+        document.body.appendChild(btnToGroup);
         this.addRedirectOnButtons(
-            {button: "group-card-1", nextPage: "group-page", pagePath: "/group"}
+            {button: "to-group-btn-redirect", nextPage: "group-page", pagePath: "/group"}
         );
+
         console.log("course")
     }
 
@@ -30,7 +36,20 @@ export default class OrganizationPage extends Page {
     render(id, resp) {
         document.getElementById("to-course-btn-redirect").click();
         debugLog("ID ORGANIZATION = " + id);
+        console.log(resp);
         document.querySelector("#org-title span").innerHTML = resp.name;
+        let groups = resp.groups;
+        console.log(groups);
+        let groups_len = groups.length;
+        let table_groups = document.getElementById("table-groups-in-org");
+        table_groups.innerHTML = "";
+        for (let i = 0; i < groups_len; i++) {
+            table_groups.innerHTML += `<tr id="group-card-${groups[i].id}" class="table-group-line">
+                <th scope="row">${i+1}</th>
+                <td>${groups[i].name}</td>
+                <td>${groups[i].created_at.split("T")[0]}</td>
+            </tr>`
+        }
     }
 
     addEventsOnButtons() {
