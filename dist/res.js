18a17b96c5881dc93002a1cf8d7fbcb2aefd1aa5
akenoq <akenoq@yandex.ru>
2018-11-18 16:30:29 +0300

    ava for default org

--- dist/res.js
+++ dist/res.js
@@ -1424,7 +1424,15 @@ class OrganizationDesk extends __WEBPACK_IMPORTED_MODULE_0__Page__["a" /* defaul
 
 
 function organizationCard(name, group_len, date) {
-    return `<img class="card-img-top" src="img/course_logo.png" alt="Card image cap">
+    let src_ava = "img/school.jpg";
+    if (name === "Школа 218")
+        src_ava = "img/org/218_ava.jpg";
+    if (name === "Школа 444")
+        src_ava = "img/org/444_ava.jpg";
+    if (name === "Инжинириум МГТУ им.Баумана")
+        src_ava = "img/org/ing_ava.png";
+
+    return `<img class="card-img-top" src=${src_ava} alt="Card image cap">
             <div class="card-body pointer">
                 <h5 class="card-title">${name}</h5>
                 <p class="card-text">${group_len} групп на курсе</p>
@@ -2501,7 +2509,6 @@ class ProfileForm extends __WEBPACK_IMPORTED_MODULE_0__modules_FormValidator_js_
     static validate(nameValue, emailValue, passwordValue, newPasswordValue, newPasswordValueRepeat) {
         return true;
     }
-
 
     setFormValues(resp) {
         this.clearFields(
